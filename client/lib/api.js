6fb55944cee78d9fb18235418f4eec9f70baa6e8
Samiaxx <cyrojohn361@gmail.com>
2026-01-25 06:28:27 -1100

    Connect frontend to Render backend

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,5 +1,4 @@
 // client/lib/api.js
-// CHANGE THIS URL AFTER RENDER DEPLOYMENT FINISHES
-const API_URL = "https://hashmarket-backend.onrender.com"; 
+const API_URL = "https://hashmarket-backend.onrender.com";
 
 export default API_URL;
