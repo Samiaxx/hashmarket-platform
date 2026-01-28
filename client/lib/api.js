93839f2b0886421e449dbd5cf574786b40adbe23
Samiaxx <cyrojohn361@gmail.com>
2026-01-28 05:55:45 -1100

    fix: remove localhost fallback for production

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,13 +1,6 @@
-# --- DATABASE CONFIG ---
-MONGO_URI=mongodb+srv://cyrojohn361gmail.com:Lacasera7@cluster0.cqhehkw.mongodb.net/hashmarket?retryWrites=true&w=majority&appName=Cluster0
-JWT_SECRET=superSecretKey2026
-PORT=5000
+// client/lib/api.js
 
-# --- EMAIL CONFIG ---
-EMAIL_USER=Hashmarketofficial@gmail.com
-EMAIL_PASS=iloa xkqj svad mjbw
-ADMIN_EMAIL=Hashmarketofficial@gmail.com
+// Strictly use the environment variable set in Vercel
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-# --- CLIENT URL (Production) ---
-# This ensures email verification links go to your custom domain.
-CLIENT_URL=https://hashmarket.buzz
+export default API_URL;
